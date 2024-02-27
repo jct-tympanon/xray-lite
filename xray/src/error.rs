ec89a5f6de97a74cdf71b6166ceaf39c64bf8136
Kikuo Emoto <kemoto@codemonger.io>
2024-02-27 16:54:32 +0900

    feat(xray): replace failure with thiserror
    `failure` is deprecated. `thiserror` is a good replacement.

--- xray/src/error.rs
+++ xray/src/error.rs
@@ -1,23 +1,11 @@
-use failure::Fail;
 use serde_json::Error as JsonError;
 use std::io::Error as IOError;
+use thiserror::Error;
 
-#[derive(Debug, Fail)]
+#[derive(Error, Debug)]
 pub enum Error {
-    #[fail(display = "IO Error")]
-    IO(IOError),
-    #[fail(display = "Json Error")]
-    Json(JsonError),
-}
-
-impl From<JsonError> for Error {
-    fn from(err: JsonError) -> Self {
-        Error::Json(err)
-    }
-}
-
-impl From<IOError> for Error {
-    fn from(err: IOError) -> Self {
-        Error::IO(err)
-    }
+    #[error("IO Error")]
+    IO(#[from] IOError),
+    #[error("Json Error")]
+    Json(#[from] JsonError),
 }
