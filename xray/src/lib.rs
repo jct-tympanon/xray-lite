54167a19e154d961fc096566f2fc00b3588ad90b
Kikuo Emoto <kemoto@codemonger.io>
2024-02-29 07:25:42 +0900

    feat(xray): add SubsegmentContext and SubsegmentSession
    `SubsegmentContext` represents a context within a subsegment of an
    existing segment or subsegment; e.g, Lambda invocation with X-Ray
    tracing enabled.
    
    `SubsegmentContext` implements a newly introduced trait `Context` that
    can begin a subsegment within the context.
    
    `SubsegmentSession` begins a subsegment within `Context` and closes it
    when dropped.
    
    `Client` implements `Clone`.

--- xray/src/lib.rs
+++ xray/src/lib.rs
@@ -28,25 +28,9 @@ pub use crate::{
 pub type Result<T> = StdResult<T, Error>;
 
 /// X-Ray daemon client interface
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Client {
     socket: Arc<UdpSocket>,
-}
-
-impl Default for Client {
-    /// Return a client configured to send trace data to an
-    /// address identified by a `AWS_XRAY_DAEMON_ADDRESS` env variable
-    /// or `127.0.0.1:2000`
-    fn default() -> Self {
-        // https://docs.aws.amazon.com/lambda/latest/dg/lambda-x-ray.html
-        // todo documment error handling
-        let addr: SocketAddr = env::var("AWS_XRAY_DAEMON_ADDRESS")
-            .ok()
-            .and_then(|value| value.parse::<SocketAddr>().ok())
-            .unwrap_or_else(|| ([127, 0, 0, 1], 2000).into());
-
-        Client::new(addr).expect("failed to connect to socket")
-    }
 }
 
 impl Client {
@@ -60,6 +44,21 @@ impl Client {
         socket.set_nonblocking(true)?;
         socket.connect(&addr)?;
         Ok(Client { socket })
+    }
+
+    /// Creates a new X-Ray client from the Lambda environment variable.
+    ///
+    /// The following environment variable must be set:
+    /// - `AWS_XRAY_DAEMON_ADDRESS`: X-Ray daemon address
+    ///
+    /// Please refer to the [AWS documentation](https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime)
+    /// for more details.
+    pub fn from_lambda_env() -> Result<Self> {
+        let addr: SocketAddr = env::var("AWS_XRAY_DAEMON_ADDRESS")
+            .map_err(|_| Error::MissingEnvVar(&"AWS_XRAY_DAEMON_ADDRESS"))?
+            .parse::<SocketAddr>()
+            .map_err(|e| Error::BadConfig(format!("invalid X-Ray daemon address: {e}")))?;
+        Client::new(addr)
     }
 
     #[inline]
@@ -81,6 +80,111 @@ impl Client {
     {
         self.socket.send(&Self::packet(data)?)?;
         Ok(())
+    }
+}
+
+/// Context.
+pub trait Context {
+    /// Enters in a new subsegment.
+    ///
+    /// [`SubsegmentSession`] records the end of the subsegment when it is
+    /// dropped.
+    fn enter_subsegment(&self, name: String) -> SubsegmentSession;
+}
+
+/// Context as a subsegment of an existing segment.
+#[derive(Debug)]
+pub struct SubsegmentContext {
+    client: Client,
+    header: Header,
+    name_prefix: String,
+}
+
+impl SubsegmentContext {
+    /// Creates a new context from the Lambda environment variable.
+    ///
+    /// The following environment variable must be set:
+    /// - `_X_AMZN_TRACE_ID`: AWS X-Ray trace ID
+    ///
+    /// Please refer to the [AWS documentation](https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime)
+    /// for more details.
+    pub fn from_lambda_env(client: Client) -> Result<Self> {
+        let header = lambda::header()?;
+        Ok(Self {
+            client,
+            header,
+            name_prefix: "".to_string(),
+        })
+    }
+
+    /// Updates the context with a given name prefix.
+    pub fn with_name_prefix(self, prefix: impl Into<String>) -> Self {
+        Self {
+            client: self.client,
+            header: self.header,
+            name_prefix: prefix.into(),
+        }
+    }
+}
+
+impl Context for SubsegmentContext {
+    fn enter_subsegment(&self, name: String) -> SubsegmentSession {
+        SubsegmentSession::new(
+            self.client.clone(),
+            &self.header,
+            format!("{}{}", self.name_prefix, name),
+        )
+    }
+}
+
+/// Subsegment session.
+pub enum SubsegmentSession {
+    /// Entered subsegment.
+    Entered {
+        client: Client,
+        header: Header,
+        subsegment: Subsegment,
+    },
+    /// Failed subsegment.
+    Failed,
+}
+
+impl SubsegmentSession {
+    fn new(client: Client, header: &Header, name: impl Into<String>) -> Self {
+        let subsegment = Subsegment::begin(
+            header.trace_id.clone(),
+            header.parent_id.clone(),
+            name,
+        );
+        match client.send(&subsegment) {
+            Ok(_) => Self::Entered {
+                client,
+                header: header.with_parent_id(subsegment.id.clone()),
+                subsegment,
+            },
+            Err(_) => Self::Failed,
+        }
+    }
+
+    /// Returns the `x-amzn-trace-id` header value.
+    pub fn x_amzn_trace_id(&self) -> Option<String> {
+        match self {
+            Self::Entered { header, .. } => Some(header.to_string()),
+            Self::Failed => None,
+        }
+    }
+}
+
+impl Drop for SubsegmentSession {
+    fn drop(&mut self) {
+        match self {
+            Self::Entered { client, subsegment, .. } => {
+                subsegment.end();
+                let _ = client.send(subsegment)
+                    .map_err(|e| eprintln!("failed to end subsegment: {e}"));
+            }
+            Self::Failed => (),
+        }
     }
 }
 
