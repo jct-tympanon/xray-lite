9fded056c2d52a32e5e2372c369132a9bf97f262
Kikuo Emoto <kemoto@codemonger.io>
2024-02-29 08:13:13 +0900

    fix(xray): examples failed on test
    Fixes the issue that `cargo test` failed to build `examples` due to
    missing `default` of `Client`. Uses `Client::from_lambda_env` instead.

--- xray/examples/print.rs
+++ xray/examples/print.rs
@@ -1,7 +1,7 @@
 use xray;
 
 fn main() {
-    let _client = xray::Client::default();
+    let _client = xray::Client::from_lambda_env();
     println!("{}", xray::TraceId::new());
     println!("{}", xray::SegmentId::new());
 }
